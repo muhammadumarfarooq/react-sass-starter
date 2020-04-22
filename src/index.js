322904227a1607f9a43010af86cb2c1c8e20143e
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-04-22 10:24:30 +0500

    styles added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// Fontawesome
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// SASS
+import "./styles/main.scss";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
